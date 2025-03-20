e866c6545ed2e89a1ed2e1ed5d3e8f88c929cd49
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-03-20 20:32:56 -0300

    Update syonet-crm.php

--- syonet-crm.php
+++ syonet-crm.php
@@ -8,15 +8,12 @@
 
 if(!defined('ABSPATH') exit;
 
-// Código do plugin vai aqui
 define('FORMULARIO_SYONET_DIR', plugin_dir_path(__FILE__));
 define('FORMULARIO_SYONET_URL', plugin_dir_url(__FILE__) . '/');
 
 require_once plugin_dir_path(__FILE__) . 'includes/shortcodes/form-render.php';
 require_once plugin_dir_path(__FILE__) . 'process_form.php';
 
-
-// Inclui os arquivos necessários
 require_once FORMULARIO_SYONET_DIR . 'includes/options-page/core.php';
 require_once FORMULARIO_SYONET_DIR . 'includes/post-type/post-type.php';
 require_once FORMULARIO_SYONET_DIR . 'includes/taxonomies/register-custom-taxonomies.php';
@@ -27,10 +24,8 @@ require_once FORMULARIO_SYONET_DIR . 'includes/post-type/envios.php';
 require_once FORMULARIO_SYONET_DIR . 'builders/core.php';
 require_once FORMULARIO_SYONET_DIR . 'update-handler.php';
 
-// Adiciona a página de opções
 add_action('admin_menu', 'syonet_add_options_page');
 
-// Adiciona o script na página de opções
 add_action('admin_enqueue_scripts', function() {
     wp_enqueue_script('option-page-change', FORMULARIO_SYONET_URL . 'assets/js/option-page-change.js', array(), '1.0', true);
 });
