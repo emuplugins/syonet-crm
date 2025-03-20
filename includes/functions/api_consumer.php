edf4e5abb8ac6825b144bd3d34ee04624daf1bfb
Emu Plugins <147978343+emuplugins@users.noreply.github.com>
2025-03-20 20:34:29 -0300

    Update api_consumer.php

--- includes/functions/api_consumer.php
+++ includes/functions/api_consumer.php
@@ -1,38 +1,32 @@
 <?php
 
-// Função para enviar dados para a API usando Bearer Token
+if ( ! defined('ABSPATH')) exit;
+
 function sendDataToApi($url, $username, $password, $jsonData) {
-    // Codifica as credenciais em Base64
+    
     $credentials = base64_encode("$username:$password");
 
-    // Inicializando a requisição cURL
     $ch = curl_init($url);
-
-    // Configurando as opções do cURL
+    
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_POST, true);
     curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         'Content-Type: application/json',
-        'Authorization: Basic ' . $credentials // Usando a autenticação básica
+        'Authorization: Basic ' . $credentials
     ]);
 
-    // Executando a requisição e capturando a resposta
     $response = curl_exec($ch);
 
-    // Verificando se houve erro na requisição
     if (curl_errno($ch)) {
-        // Caso ocorra erro, captura e exibe o erro
         $error_msg = curl_error($ch);
         curl_close($ch);
-        return "cURL Error: " . $error_msg;  // Retorna o erro
+        return "cURL Error: " . $error_msg;
     }
 
-    // Fechando a conexão cURL
     curl_close($ch);
     if ( ! $response) {
         return "Erro ao enviar dados para a API";
     }
-    // Retorna a resposta da API
     return $response;
 }
